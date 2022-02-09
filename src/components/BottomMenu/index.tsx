08b095883bee7b17fc638895b335362f13d201b8
ellandor <66072698+ellandor@users.noreply.github.com>
2022-02-09 13:03:10 -0300

    Hide bottom menu according to the screen scroll

--- src/components/BottomMenu/index.tsx
+++ src/components/BottomMenu/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect } from 'react'
 
 import { BsMoonStarsFill, BsFillSunFill } from 'react-icons/bs'
 
@@ -10,8 +10,28 @@ type Props = {
 }
 
 export function BottomMenu({ theme, setTheme }: Props) {
+    useEffect(() => {
+        const maxScrollY = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        const menu = document.querySelector('.bottom-menu') as any
+
+        function a(e: any) {
+            const scrollY = window.scrollY
+
+            if (scrollY > 10 && scrollY < maxScrollY - 10) {
+                menu.style.top = 'calc(100% - 7vh)'
+            } else {
+                menu.style.top = '100%'
+            }
+        }
+
+        document.addEventListener('scroll', a)
+        return () => {
+            document.removeEventListener('scroll', a)
+        }
+    }, [])
+
     return (
-        <div className='menu fixed w-screen h-[7vh] bg-[#e6e9ed] dark:bg-_dark-200 top-[calc(100%-7vh)] z-50 rounded-t-lg lg:hidden'>
+        <div className='bottom-menu fixed w-screen h-[7vh] bg-[#e6e9ed] dark:bg-_dark-200 top-full z-50 rounded-t-lg lg:hidden transition-[top_0.2s]'>
             <ul className='flex justify-around items-center w-full h-full px-4'>
                 <li onClick={() => setTheme(theme => (theme === 'light' ? 'dark' : 'light'))}>
                     {theme === 'light' ? (
